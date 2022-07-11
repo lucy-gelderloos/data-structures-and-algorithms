1deef1c0b2b29cc7fa314bdacaa2cbcde02e51de
Lucy Gelderloos <lucygel@gmail.com>
2022-07-11 15:45:24 -0700

    Challenge 7 passes

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -141,7 +141,22 @@ Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
 
 const fizzbuzz = (arr) => {
-  // Solution code here...
+  let fizzbuzzArray = [];
+  arr.forEach((element) => {
+    if(element % 3 === 0 && element % 5 === 0) {
+      fizzbuzzArray.push(`Fizz Buzz`);
+    }
+    else if(element % 3 === 0) {
+      fizzbuzzArray.push(`Fizz`);
+    }
+    else if(element % 5 === 0) {
+      fizzbuzzArray.push(`Buzz`);
+    }
+    else {
+      fizzbuzzArray.push(element);
+    }
+  });
+  return(fizzbuzzArray);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -195,7 +210,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
