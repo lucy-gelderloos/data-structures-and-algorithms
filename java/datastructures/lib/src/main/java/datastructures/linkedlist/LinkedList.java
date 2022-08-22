0307142fd9577978612026ec334f1d7630480a77
Lucy Gelderloos <lucygel@gmail.com>
2022-08-21 22:07:32 -0700

    Create LinkedList class

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -1,28 +1,47 @@
 package datastructures.linkedlist;
 
-
-//  https://www.javatpoint.com/java-program-to-create-a-singly-linked-list-of-n-nodes-and-count-the-number-of-nodes
 public class LinkedList
 {
-  public Node head = null;
-//  public Node tail = null;
+  public Node head;
 
-  public void addNode(String value) {
+  public LinkedList() {
+    // creates new list with head null
+    this.head = null;
+  }
+
+  //https://www.geeksforgeeks.org/linked-list-set-2-inserting-a-node/
+  public void insert(String value) {
+    // creates new node using Node class
     Node newNode = new Node(value);
+    // sets pointer of new node to point at current head
+    newNode.setNext(this.head);
+    // makes new node new head
+    this.head = newNode;
+  }
 
-    //Checks if the list is empty
-    if(head == null) {
-      //If list is empty, head will point to new node
-      head = newNode;
-    }
-    else {
-
-    }
+  public boolean includes(String value) {
+    // start at the head of the list
+    Node thisNode = this.head;
+    // if this node is null, exit
+    while (thisNode != null) {
+      // get value of this node & return true if value matches argument
+      if (thisNode.getValue() == value) {
+        return true;
+      }
+      // move to the next node
+      thisNode = thisNode.getNext();
+      }
+      return false;
   }
 
   @Override
-  public String toString()
-    {
-        return "null";  // TODO: implement me
+  public String toString() {
+    String contents = "";
+    // same loop as above, except add the value to the string instead of checking against an argument
+    Node thisNode = this.head;
+    while (thisNode != null) {
+      contents += "{" + thisNode.getValue() + "} -> ";
+    }
+      return contents + "NULL";
     }
 }
