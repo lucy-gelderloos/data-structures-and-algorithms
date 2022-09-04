e907ed7dce25f43874e0d0aceb794bbb68a1cf68
Lucy Gelderloos <lucygel@gmail.com>
2022-09-04 11:07:09 -0700

    Slept on it; moved to own package; added BinaryTree traversal methods

---
+++ java/datastructures/lib/src/main/java/datastructures/trees/BinaryTree.java
@@ -0,0 +1,81 @@
+package datastructures.trees;
+
+import datastructures.linkedlist.Stack;
+
+import java.util.ArrayList;
+
+import static java.util.Objects.isNull;
+
+public class BinaryTree {
+  private Node root;
+
+  public BinaryTree() {
+    this.root = null;
+  }
+
+  public ArrayList<Integer> getValuesPreorder() {
+    ArrayList<Integer> values = new ArrayList<>();
+    preOrder(this.root, values);
+    return values;
+  }
+
+  // https://www.techiedelight.com/preorder-tree-traversal-iterative-recursive/
+  public void preOrder(Node root, ArrayList<Integer> values) {
+    if (root == null) {
+      return;
+      }
+    values.add(root.getValue());
+    if(!isNull(root.getLeft())) {
+      preOrder(root.getLeft(), values);
+    }
+    if(!isNull(root.getRight())) {
+      preOrder(root.getRight(), values);
+    }
+  }
+
+  public ArrayList<Integer> getValuesInOrder() {
+    ArrayList<Integer> values = new ArrayList<>();
+    inOrder(this.root, values);
+    return values;
+  }
+
+  public void inOrder(Node root, ArrayList<Integer> values) {
+    if (root == null) {
+      return;
+      }
+    if(!isNull(root.getLeft())) {
+      preOrder(root.getLeft(), values);
+    }
+    values.add(root.getValue());
+    if(!isNull(root.getRight())) {
+      preOrder(root.getRight(), values);
+    }
+  }
+
+  public ArrayList<Integer> getValuesPostOrder() {
+    ArrayList<Integer> values = new ArrayList<>();
+    inOrder(this.root, values);
+    return values;
+  }
+
+  public void postOrder(Node root, ArrayList<Integer> values) {
+    if (root == null) {
+      return;
+      }
+    if(!isNull(root.getLeft())) {
+      preOrder(root.getLeft(), values);
+    }
+    if(!isNull(root.getRight())) {
+      preOrder(root.getRight(), values);
+    }
+    values.add(root.getValue());
+  }
+
+  public Node getRoot() {
+    return root;
+  }
+
+  public void setRoot(Node root) {
+    this.root = root;
+  }
+}
