d827df41614753406650dfc70c3b5b2599fb957e
Lucy Gelderloos <lucygel@gmail.com>
2022-09-25 17:26:37 -0700

    Update readme; refactor tests

--- java/datastructures/lib/src/test/java/datastructures/hashmap/HashMapTest.java
+++ java/datastructures/lib/src/test/java/datastructures/hashmap/HashMapTest.java
@@ -12,7 +12,11 @@ public class HashMapTest {
 
     assertNull(testHashMap.get(3));
     assertNull(testHashMap.getKeys());
+  }
 
+  @Test
+  public void testAddToHashMap() {
+    HashMap<Integer, String> testHashMap = new HashMap<>(5);
     testHashMap.set(3,"Value3");
     testHashMap.set(7,"Value7");
     testHashMap.set(56,"Value56");
@@ -22,14 +26,50 @@ public class HashMapTest {
     assertTrue(testHashMap.has(7));
     assertTrue(testHashMap.has(56));
     assertTrue(testHashMap.has(-6));
+  }
+
+  @Test
+  public void testGet() {
+    HashMap<Integer, String> testHashMap = new HashMap<>(5);
+    testHashMap.set(3,"Value3");
+    testHashMap.set(7,"Value7");
+    testHashMap.set(56,"Value56");
+    testHashMap.set(-6,"Value-6");
+
 
     assertEquals("Value3",testHashMap.get(3));
     assertEquals("Value-6",testHashMap.get(-6));
-    assertNotNull(testHashMap.getKeys().contains(56));
-    assertNotNull(testHashMap.getKeys().contains(-6));
-
-    testHashMap.set(3,"NewValue3");
-
-    assertEquals("NewValue3",testHashMap.get(3));
+    assertNull(testHashMap.get(10));
   }
+
+  @Test
+  public void testGetKeys() {
+    HashMap<Integer, String> testHashMap = new HashMap<>(5);
+    testHashMap.set(3,"Value3");
+    testHashMap.set(7,"Value7");
+    testHashMap.set(56,"Value56");
+    testHashMap.set(-6,"Value-6");
+
+    assertTrue(testHashMap.getKeys().contains(56));
+    assertTrue(testHashMap.getKeys().contains(-6));
+  }
+
+  @Test
+  public void testCollision() {
+    HashMap<Integer, String> testHashMap = new HashMap<>(1);
+    testHashMap.set(3,"Value3");
+    testHashMap.set(7,"Value7");
+    testHashMap.set(56,"Value56");
+    testHashMap.set(-6,"Value-6");
+
+    assertEquals(testHashMap.hash(3), testHashMap.hash(7));
+    assertTrue(testHashMap.getKeys().contains(56));
+    assertEquals("Value-6",testHashMap.get(-6));
+
+
+    testHashMap.set(-6,"NewValue-6");
+
+    assertEquals("NewValue-6",testHashMap.get(-6));
+  }
+
 }
