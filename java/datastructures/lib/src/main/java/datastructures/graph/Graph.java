4f3fead9186db37624b0a6a9eb8fa2180978e338
Lucy Gelderloos <lucygel@gmail.com>
2022-10-02 20:16:21 -0700

    Rename adjacencyLists

--- java/datastructures/lib/src/main/java/datastructures/graph/Graph.java
+++ java/datastructures/lib/src/main/java/datastructures/graph/Graph.java
@@ -5,20 +5,20 @@ import java.util.LinkedList;
 
 public class Graph<T extends Comparable<? super T>> implements Comparable<Graph<T>> {
 
-  public HashMap<Node<T>, LinkedList<Edge<T>>> adjacencyLists;
+  public HashMap<Node<T>, LinkedList<Edge<T>>> adjacencyList;
   private int nodeCount = 0;
 
   public Graph(int maxNodes) {
-    adjacencyLists = new HashMap<>(maxNodes);
+    adjacencyList = new HashMap<>(maxNodes);
   }
 
   public Node<T> addNode(T value) throws Exception {
     Node<T> newNode = new Node<>(value);
-    if(adjacencyLists.containsKey(newNode)) {
+    if(adjacencyList.containsKey(newNode)) {
       throw new Exception("This value is already in the graph.");
     }
     LinkedList<Edge<T>> edgeList = new LinkedList<>();
-    adjacencyLists.put(newNode,edgeList);
+    adjacencyList.put(newNode,edgeList);
     nodeCount++;
     return newNode;
   }
@@ -28,25 +28,25 @@ public class Graph<T extends Comparable<? super T>> implements Comparable<Graph<
   }
 
   public void addEdge(Node<T> start, Node<T> destination, int weight) throws Exception {
-    if(!adjacencyLists.containsKey(start) || !adjacencyLists.containsKey(destination)) {
+    if(!adjacencyList.containsKey(start) || !adjacencyList.containsKey(destination)) {
       throw new Exception("Both nodes must be in the graph.");
     }
     Edge<T> newEdge = new Edge<>(destination,weight);
-    LinkedList<Edge<T>> edgeList = adjacencyLists.get(start);
+    LinkedList<Edge<T>> edgeList = adjacencyList.get(start);
     edgeList.add(newEdge);
-    adjacencyLists.put(start,edgeList);
+    adjacencyList.put(start,edgeList);
   }
 
   public LinkedList<Node<T>> getNodes() {
     LinkedList<Node<T>> nodeList = new LinkedList<>();
-    for(Node<T> node : adjacencyLists.keySet()) {
+    for(Node<T> node : adjacencyList.keySet()) {
       nodeList.add(node);
     }
     return nodeList;
   }
 
   public LinkedList<Edge<T>> getNeighbors(Node<T> node) {
-    return adjacencyLists.get(node);
+    return adjacencyList.get(node);
   }
 
   public int size() {
@@ -69,12 +69,12 @@ public class Graph<T extends Comparable<? super T>> implements Comparable<Graph<
 //    return "";
 //  }
 
-  public HashMap<Node<T>, LinkedList<Edge<T>>> getAdjacencyLists() {
-    return adjacencyLists;
+  public HashMap<Node<T>, LinkedList<Edge<T>>> getAdjacencyList() {
+    return adjacencyList;
   }
 
-  public void setAdjacencyLists(HashMap<Node<T>, LinkedList<Edge<T>>> adjacencyLists) {
-    this.adjacencyLists = adjacencyLists;
+  public void setAdjacencyList(HashMap<Node<T>, LinkedList<Edge<T>>> adjacencyList) {
+    this.adjacencyList = adjacencyList;
   }
 
   public int getNodeCount() {
